8c013ad8f1208d43048ceff540e878b97f7c6185
Kqzz <48300938+Kqzz@users.noreply.github.com>
2021-02-25 16:43:07 -0500

    add init file so that you can import mcsniperpy

---
+++ mcsniperpy/__init__.py
@@ -0,0 +1,5 @@
+from .sniper import Sniper
+from .util import logs_manager, ping_tester, request_manager, name_system, utils
+from .util import classes
+
+__all__ = (classes, sniper, logs_manager, ping_tester, request_manager, name_system, utils)

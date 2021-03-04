280d41a3f5cafabc798b1f5814a895c8d4bbaaef
kqzz <48300938+Kqzz@users.noreply.github.com>
2021-03-04 17:20:06 -0500

    update version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="MCsniperPY",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version='0.15',
+    version='0.15.1',
     py_modules=['mcsniperpy'],
     license='MIT',
     author='Kqzz',

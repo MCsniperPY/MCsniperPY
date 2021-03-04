456fa54264c9a9008a93b77e2c30abb2751b4880
kqzz <48300938+Kqzz@users.noreply.github.com>
2021-03-04 17:05:50 -0500

    update setup.py for upload

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="MCsniperPY",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version='0.15',
     py_modules=['mcsniperpy'],
+    license='MIT',
+    author='Kqzz',
+    url='https://github.com/MCsniperPY/MCsniperPY',
     install_requires=[
         'typer',
         'aiohttp',
         'colorama',
-        'requests',
-        'python-dateutil',
         'bs4'
     ],
     entry_points={
         'console_scripts': ['mcsniperpy=mcsniperpy.cli:cli']
     },
-    python_requires='>=3.7'
+    python_requires='>=3.8',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',  # Again, pick a license
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
 )

6ebb4e63182f5c0eb6fab8ec4a31c291cacbaa8e
overestimate <58041173+overestimate@users.noreply.github.com>
2021-03-04 21:29:38 -0600

    amazing job kqzz managed to put config in the install location

--- mcsniperpy/util/classes/config.py
+++ mcsniperpy/util/classes/config.py
@@ -10,7 +10,7 @@ class BackConfig:
     """"""
 
     def __init__(self):
-        config_path = os.path.join(dirname(dirname(dirname(__file__))), "data", "config.ini")
+        config_path = os.path.join(os.getcwd(), "data", "config.ini")
         config = configparser.ConfigParser()
         config.read(config_path)
 
@@ -41,8 +41,9 @@ def populate_configs():
         close(1)
     config = configparser.ConfigParser()
     config['sniper'] = {"init_path": os.getcwd()}
-
-    with open(os.path.join(dirname(dirname(dirname(__file__))), "data", "config.ini"), "w") as f:
+    if not os.path.isdir(os.path.join(os.getcwd(), "data")):
+        os.mkdir(os.path.join(os.getcwd(), "data"))
+    with open(os.path.join(os.getcwd(), "data", "config.ini"), "w") as f:
         config.write(f)
 
     if os.path.isfile(os.path.join(config['sniper']['init_path'], "accounts.txt")):

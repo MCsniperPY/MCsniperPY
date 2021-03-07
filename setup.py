c4a9590a333b6210de410e57ead753a62d57ff5d
kqzz <48300938+Kqzz@users.noreply.github.com>
2021-03-07 11:49:27 -0500

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="MCsniperPY",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version='0.16.1',
+    version='0.16.2',
     py_modules=['mcsniperpy'],
     license='MIT',
     author='Kqzz',

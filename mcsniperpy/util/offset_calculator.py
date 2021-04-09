03e5229dc5c53044761652eb4783110f683e51d3
kqzz <48300938+Kqzz@users.noreply.github.com>
2021-04-09 18:00:25 -0400

    fix linting on offset calc file

--- mcsniperpy/util/offset_calculator.py
+++ mcsniperpy/util/offset_calculator.py
@@ -12,7 +12,7 @@ from mcsniperpy.util.logs_manager import Logger as log
 
 
 class OffsetCalculator:
-    def __init__(self, req_count=2, accuracy=20, aim_for=.15, do_log=True):
+    def __init__(self, req_count=2, accuracy=20, aim_for=0.15, do_log=True):
         self.session = request_manager.RequestManager(
             None  # aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=300),headers={})
         )
@@ -39,8 +39,10 @@ class OffsetCalculator:
             droptime = math.floor(time.time() + 3)
             self.accounts = util.parse_accs_string("email:pass")
             if self.do_log:
-                log.info(f"testing offset {self.offset} in {round(droptime - time.time())} seconds")
-            res = await self.offset_test(droptime, 'test', self.offset)
+                log.info(
+                    f"testing offset {self.offset} in {round(droptime - time.time())} seconds"
+                )
+            res = await self.offset_test(droptime, "test", self.offset)
             if res is not True:
                 self.offset += res
             else:
@@ -48,7 +50,9 @@ class OffsetCalculator:
 
     async def offset_test(self, droptime, target, offset):
 
-        pre_snipe_coroutines = [acc.snipe_connect() for _ in range(self.req_count) for acc in self.accounts]  # For later use
+        pre_snipe_coroutines = [
+            acc.snipe_connect() for _ in range(self.req_count) for acc in self.accounts
+        ]  # For later use
 
         now = time.time()
         time_until_connect = 0 if now > (droptime - 20) else (droptime - 20) - now
@@ -58,20 +62,33 @@ class OffsetCalculator:
         await asyncio.gather(*pre_snipe_coroutines)  # Connects
 
         snipe_coroutines = [
-            acc.snipe(acc.readers_writers[i][1], do_log=False) for i in range(self.req_count) for acc in self.accounts
+            acc.snipe(acc.readers_writers[i][1], do_log=False)
+            for i in range(self.req_count)
+            for acc in self.accounts
         ]  # Send requests
 
         while time.time() < droptime - offset / 1000:
-            await asyncio.sleep(0.00001)  # bad timing solution but it's fairly accurate
-        # According to my tests this was .0004 seconds late while the other method (asyncio.sleep) was .004 seconds late
+            await asyncio.sleep(0.00001)  # bad timing solution but
+            # it's fairly accurate
+
+        # According to my tests this was .0004 seconds late while
+        # the other method (asyncio.sleep) was .004 seconds late
 
         start = time.perf_counter()
         await asyncio.gather(*snipe_coroutines)  # Sends the snipe requests
         end = time.perf_counter()
 
         responses = await asyncio.gather(
-            *[acc.snipe_read(target, acc.readers_writers[i][0], acc.readers_writers[i][1], do_log=self.do_log
-                             ) for i in range(self.req_count) for acc in self.accounts]
+            *[
+                acc.snipe_read(
+                    target,
+                    acc.readers_writers[i][0],
+                    acc.readers_writers[i][1],
+                    do_log=self.do_log,
+                )
+                for i in range(self.req_count)
+                for acc in self.accounts
+            ]
         )  # Reads the responses
         # I swear I didn't try to make it ugly
 
@@ -79,19 +96,23 @@ class OffsetCalculator:
 
         target_time = droptime + self.aim_for  # Find the actual target time
 
-        average_time = mean([req_time for _, _, req_time in responses])  # Finds the mean request time
+        average_time = mean(
+            [req_time for _, _, req_time in responses]
+        )  # Finds the mean request time
 
         diff = -(target_time - average_time) * 1000
         if self.accuracy > diff > 0:
             if self.do_log:
-                log.info(f"{color.white}[{color.green}success{color.white}]{color.reset} {offset} is a good offset!")
+                log.info(
+                    f"{color.white}[{color.green}success{color.white}] "
+                    "{color.reset} {offset} is a good offset!"
+                )
             return True
-        else:
-            return round(diff)
+
+        return round(diff)
 
     def on_shutdown(self):
         if self.session.session is not None:
             asyncio.run(self.session.session.close())
         if self.log is not None:
             self.log.shutdown()
-

92838e6eaccc3ab045e33534c32130cec8df90ae
chrommie <83525915+chrommie@users.noreply.github.com>
2021-07-14 17:03:01 -0400

    Update name_system.py

--- mcsniperpy/util/name_system.py
+++ mcsniperpy/util/name_system.py
@@ -13,6 +13,7 @@ async def peet_timing(username: str, session: RequestManager):
     resp, _, resp_json = await session.get(
         f"https://drops.peet.ws/droptime?name={username}"
     )
+    print(resp_json)
     if resp.status < 300:
         log.info(
             f"sniping {color.cyan}{username}{color.reset} @ {datetime.fromtimestamp(resp_json['UNIX'])}"
